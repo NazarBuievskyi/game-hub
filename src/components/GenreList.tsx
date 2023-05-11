467d0fd8d2e5985be43cf723052cb718e83ff10c
Your Name <nbuievskyi@gmail.com>
2023-05-11 19:40:02 +0200

    Refactor game grid

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -18,7 +18,7 @@ function GenreList({selectedGenre, onSelectGenre}: Props) {
 
     return (
         <>
-            <Heading fontSize='2xl' marginBottom='3'>Genres</Heading>
+            <Heading fontSize='2xl' marginBottom='3' marginTop={10}>Genres</Heading>
             <List>
                 {data.map((genre) => <ListItem key={genre.id} paddingY={'5px'}>
                     <HStack>
