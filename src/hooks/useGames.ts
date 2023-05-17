966913891e6b18a7f188c3aac0648c90435d3433
Your Name <nbuievskyi@gmail.com>
2023-05-17 22:43:42 +0200

    Refactor stale time

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,8 @@
-import {FetchResponse} from "../services/api-client";
+import APIClient, {FetchResponse} from "../services/api-client";
 import {GameQuery} from "../App";
 import {useInfiniteQuery} from "react-query";
-import APIClient from "../services/api-client";
 import {Platform} from "./usePlatforms";
+import ms from 'ms'
 
 
 const apiClient = new APIClient<Game>('/games',)
@@ -31,7 +31,7 @@ const useGames = (gameQuery: GameQuery) =>
         getNextPageParam: (lastPage, allPages) => {
             return lastPage.next ? allPages.length + 1 : undefined
         },
-        staleTime: 24 * 60 * 60 * 1000 //  24h
+        staleTime: ms('24h'),
     })
 
 export default useGames
