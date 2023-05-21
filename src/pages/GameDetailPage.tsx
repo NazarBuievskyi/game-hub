b15b20e182e5a3682d5535052301e2909de4c2b0
Your Name <nbuievskyi@gmail.com>
2023-05-21 14:54:07 +0200

    Build game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -5,6 +5,7 @@ import {Heading, Spinner} from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GameTrailer from "../components/GameTrailer";
+import GameScreenshots from "../components/GameScreenshots";
 
 function GameDetailPage() {
     const {slug} = useParams()
@@ -20,6 +21,7 @@ function GameDetailPage() {
             <ExpandableText>{game.description_raw}</ExpandableText>
             <GameAttributes game={game}/>
             <GameTrailer gameId={game.id}/>
+            <GameScreenshots gameId={game.id}/>
         </>
     );
 }
