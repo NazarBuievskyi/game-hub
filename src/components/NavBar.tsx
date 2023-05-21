9cbf82a4c374b2b3524f9e2d863ee8eeeadb7312
Your Name <nbuievskyi@gmail.com>
2023-05-21 15:05:09 +0200

    Makes the navBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,12 +3,13 @@ import {HStack, Image} from "@chakra-ui/react";
 import logo from '../assets/logo.svg'
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import {Link} from "react-router-dom";
 
 
 function NavBar() {
     return (
-        <HStack  padding={'10px'}>
-            <Image src={logo} boxSize='60px'/>
+        <HStack padding={'10px'}>
+            <Link to={'/'}><Image src={logo} boxSize='60px' objectFit={'cover'}/></Link>
             <SearchInput/>
             <ColorModeSwitch/>
         </HStack>
